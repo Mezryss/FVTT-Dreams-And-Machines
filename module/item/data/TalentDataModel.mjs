4f9ef52426b1378105400f41fcdb6effbffee1df
Mezryss <mezryss@minalien.dev>
2023-07-07 20:47:44 -0700

    Starter Box: Talents

--- module/item/data/TalentDataModel.mjs
+++ module/item/data/TalentDataModel.mjs
@@ -4,11 +4,20 @@ import ItemDescription from './templates/ItemDescription.mjs';
  * Data model representing Talents.
  *
  * @mixes ItemDescription
+ *
+ * @property {string} archetype Archetype the talent is associated with.
  */
 export default class ItemDataModel extends foundry.abstract.TypeDataModel {
 	static defineSchema() {
+		const fields = foundry.data.fields;
+
 		return {
 			...ItemDescription(),
+
+			archetype: new fields.StringField({
+				initial: '',
+				nullable: false,
+			}),
 		};
 	}
 }
